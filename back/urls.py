e0ad4e74a9053f00b66726c8054d407b73a3f469
microMur <viktena@mail.ru>
2018-12-12 21:27:18 +0300

    add other views, changed html-files

--- back/urls.py
+++ back/urls.py
@@ -4,4 +4,10 @@ from back import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+	path('login', views.login, name='login'),
+	path('profil', views.profil, name='profil'),
+	path('discipline', views.discipline, name='discipline'),
+	path('discipline_change', views.discipline_change, name='discipline_change'),
+	path('registration', views.registration, name='registration'),
 ]
+
--- back/views.py
+++ back/views.py
@@ -2,7 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+
 # Create your views here.
+
 def index(request):
     template = loader.get_template('wiki-1-glavn.html')
+    return HttpResponse(template.render({},request))
+
+def login(request):
+    template = loader.get_template('wiki-1-vhod.html')
+    return HttpResponse(template.render({},request))
+	
+def profil(request):
+    template = loader.get_template('wiki-1-profil.html')
+    return HttpResponse(template.render({},request))
+	
+def discipline(request):
+    template = loader.get_template('wiki-1-discipl.html')
+    return HttpResponse(template.render({},request))
+
+def discipline_change(request):
+    template = loader.get_template('wiki-1-discipl_red.html')
+    return HttpResponse(template.render({},request))
+
+def registration(request):
+    template = loader.get_template('wiki-1-registr.html')
     return HttpResponse(template.render({},request))
