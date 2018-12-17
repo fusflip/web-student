2e72a4f84e41360ae97239a535c44c8a53a3f27f
microMur <viktena@mail.ru>
2018-12-17 22:03:14 +0300

    add models in admin.py

--- back/admin.py
+++ back/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Paper, Settings, Subject 
+
+admin.site.register(Paper) 
+admin.site.register(Settings) 
+admin.site.register(Subject)
